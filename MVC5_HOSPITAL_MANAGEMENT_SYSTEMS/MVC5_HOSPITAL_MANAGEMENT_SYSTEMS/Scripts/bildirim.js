43d9bb785ae7ea4a3179fa6648d8e3b72e116eea
NuhDemir <drmaa217@gmail.com>
2024-06-30 13:06:29 +0300

    scripts

---
+++ MVC5_HOSPITAL_MANAGEMENT_SYSTEMS/MVC5_HOSPITAL_MANAGEMENT_SYSTEMS/Scripts/bildirim.js
@@ -0,0 +1,26 @@
+﻿toastr.options.preventDuplicates = false;
+toastr.options.closeButton = true;
+toastr.options.escapeHtml = true;
+
+//// Display a warning toast, with no title
+//toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
+
+//// Display a success toast, with a title
+//toastr.success('Have fun storming the castle!', 'Miracle Max Says')
+
+//// Display an error toast, with a title
+//toastr.error('I do not think that word means what you think it means.', 'Inconceivable!')
+
+//// Immediately remove current toasts without using animation
+//toastr.remove()
+
+//// Remove current toasts using animation
+//toastr.clear()
+
+//// Override global options
+//toastr.success('We do have the Kapua suite available.', 'Turtle Bay Resort', { timeOut: 5000 })
+// Define a callback for when the toast is shown/hidden/clicked
+//toastr.options.onShown = function () { console.log('hello'); }
+//toastr.options.onHidden = function () { console.log('goodbye'); }
+//toastr.options.onclick = function () { console.log('clicked'); }
+//toastr.options.onCloseClick = function () { console.log('close button clicked'); }
